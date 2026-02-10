3879d3954c13be79d88fd36214dd5f258a65822c
ansatmadagascar <antsamadagascar@gmail.com>
2026-02-10 16:40:15 +0300

    changing name  seeders to boutiques.sedd

--- backend/seeds/boutiques.seed.js
+++ backend/seeds/boutiques.seed.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 const Boutique = require('../models/Boutique');
 const Categorie = require('../models/Categorie');
 const SousCategorie = require('../models/SousCategorie');
-require('dotenv').config();
-
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const seedBoutiques = async () => {
   try {
+    console.log('🔹 MONGO_URI:', process.env.MONGO_URI);
+
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
