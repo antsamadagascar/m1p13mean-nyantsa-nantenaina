2cfdfa477580766c565f55c5f680cfe21e544526
ansatmadagascar <antsamadagascar@gmail.com>
2026-02-12 14:46:47 +0300

    conflict resolution part5

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -19,7 +19,7 @@ export const routes: Routes = [
     component: RegisterComponent
   },
   {
-    path: 'verify-email',
+    path: 'users/verify-email',
     component: VerifyEmailComponent
   },
 
