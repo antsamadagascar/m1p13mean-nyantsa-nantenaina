2cb8d62dcd88357a9b93f000f62d681f4c0e1f80
ansatmadagascar <antsamadagascar@gmail.com>
2026-02-25 08:28:50 +0300

    fix:udapting section sidebar

--- frontend/src/app/layouts/admin-layout/admin-layout.component.ts
+++ frontend/src/app/layouts/admin-layout/admin-layout.component.ts
@@ -62,17 +62,15 @@ export class AdminLayoutComponent implements OnInit {
         { path: '/backoffice', label: 'Tableau de bord', icon: 'fa-solid fa-chart-line', exact: true },
       ];
 
-      this.managementMenuItems = [
+     this.managementMenuItems = [
         { path: '/backoffice/users', label: 'Utilisateurs', icon: 'fa-solid fa-users', exact: false },
         { path: '/backoffice/zones', label: 'Zones', icon: 'fa-solid fa-location-dot', exact: false },
         { path: '/backoffice/emplacements', label: 'Emplacements', icon: 'fa-solid fa-door-open', exact: false },
         { path: '/backoffice/boutiques', label: 'Boutiques', icon: 'fa-solid fa-store', exact: false },
-        { path: '/backoffice/locations', label: 'Contrats & Locations', icon: 'fa-solid fa-building', exact: false },
+        { path: '/backoffice/locations', label: 'Contrats & Locations', icon: 'fa-solid fa-file-signature', exact: false },
         { path: '/backoffice/paiements', label: 'Factures & Paiements', icon: 'fa-solid fa-receipt', exact: false },
         { path: '/backoffice/products', label: 'Produits', icon: 'fa-solid fa-box', exact: false },
- 
-        // { path: '/backoffice/orders', label: 'Commandes', icon: 'fa-solid fa-cart-shopping', exact: false },
-
+        { path: '/backoffice/orders', label: 'Commandes', icon: 'fa-solid fa-cart-shopping', exact: false },
       ];
 
       // this.configMenuItems = [
