e556690fc1fd37cad397e7745161c6d3fe46d9b4
ansatmadagascar <antsamadagascar@gmail.com>
2026-02-25 22:11:19 +0300

    refractor:renaming controller

--- backend/routes/mouvement_stock.routes.js
+++ backend/routes/mouvement_stock.routes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
-const stockController = require('../controllers/stockMovementsController');
+const stockController = require('../controllers/stockMovements.controller');
 
 // Vérif rapide
 console.log('stockController:', stockController);
