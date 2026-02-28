4d1bdab804a59cbee9130b5861edb003ab98c1a2
ansatmadagascar <antsamadagascar@gmail.com>
2026-02-28 09:19:00 +0300

    feat(auth):expiration token session 24h

--- backend/utils/tokenUtils.js
+++ backend/utils/tokenUtils.js
@@ -5,7 +5,7 @@ const generateTokenSession = (userId, role) => {
   return jwt.sign(
     { id: userId, role },
     process.env.JWT_SECRET,
-    { expiresIn: '7d' }
+    { expiresIn: '24h' }
   );
 };
 
